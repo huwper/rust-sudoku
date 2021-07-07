5e47f7fa8d3d5b06b4abb2ffe467acd0530b0839
Huw Percival <huw.percival@dyson.com>
2021-07-07 12:14:30 +0100

    got my iterators to work. I think it's worse than if I used normal for loops

--- src/latin_square.rs
+++ src/latin_square.rs
@@ -1,4 +1,8 @@
-use std::{char::MAX, iter::{Chain, Map}, ops::Range};
+use std::{
+    char::MAX,
+    iter::{Chain, Map},
+    ops::Range,
+};
 
 /// solve a 2x2 latin square
 /// 4 squares
@@ -10,14 +14,14 @@ use std::{char::MAX, iter::{Chain, Map}, ops::Range};
 
 ///constraints
 
+#[derive(Clone, Copy)]
 struct RowEntry {
     number: usize,
     location: (usize, usize),
 }
 
 const ROW_LENGTH: usize = 2;
-const MAX_VAL: usize = ROW_LENGTH.pow(2);
-
+const MAX_VAL: usize = ROW_LENGTH;
 
 trait Constraint {
     fn check(&self, l: RowEntry) -> bool;
@@ -55,8 +59,19 @@ pub fn demo() {
         })
     });
 
-    let row_constraints = (0..ROW_LENGTH).map(move |n| Box::new(RowConstraint{n: n}));
-    let col_constraints = (0..ROW_LENGTH).map(move |n| Box::new(ColConstraint{n: n}) );
+    let all_constraints = || {
+        let row_constraints = (0..ROW_LENGTH).flat_map(|r| {
+            (0..MAX_VAL).map(move |n| -> Box<dyn Constraint> { Box::new(RowConstraint { n: n }) })
+        });
+        let col_constraints = (0..ROW_LENGTH).flat_map(|r| {
+            (0..MAX_VAL).map(move |n| -> Box<dyn Constraint> { Box::new(ColConstraint { n: n }) })
+        });
+        row_constraints.chain(col_constraints)
+    };
 
-    let dlx_table: Vec<Vec<bool>> = all_location_iter.map(|location| all_constraints.map(|c| c.check(location)).collect()).collect();
+    let dlx_table: Vec<Vec<bool>> = all_location_iter
+        .map(move |location| all_constraints().map(move |c| c.check(location)).collect())
+        .collect();
+
+    println!("{:?}", dlx_table);
 }
